35e587c6e581beb0449aef55ba7e7ef081e12a97
Harshul Jain <harshulgangwal02@gmail.com>
2022-12-30 12:05:24 +0530

    __name__

--- Python Functions and Modules/my_custom.py
+++ Python Functions and Modules/my_custom.py
@@ -1,2 +1,10 @@
 def greeting(name):
     return "Hello "+ name
+
+print("I am working on project.py")
+
+print("Imported Module : ", __name__)
+
+
+if __name__ == '__main__':
+    print("I am very confidential code")
---
+++ Python Functions and Modules/project.py
@@ -0,0 +1,5 @@
+import my_custom
+
+print(my_custom.greeting("Harsh"))
+
+print(__name__)
